8ab89553726727ff044ed6c78381d3e8fcaa5138
EmmanuelSteven <chizzyjiver@gmail.com>
2024-05-07 23:19:46 +0100

    updatenav

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import AllMeetUpspage from './pages/AllMeetUps';
 import NewMeetUpPage from './pages/NewMeetUps';
 import FavouritesPage from './pages/Favourites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path='/' element={<AllMeetUpspage />} />
         <Route path='/new-meetup' element={<NewMeetUpPage />} />
